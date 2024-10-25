f16ef8227b5ace13cda51954e14980a3a6a7b83f
fmacedosantos <felipemacedo986@gmail.com>
2024-10-25 16:54:36 -0300

    arquivos iniciais

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+// A pasta do projeto deve ficar em xampp/htdocs
+
+require_once __DIR__.'/core/Core.php';
+require_once __DIR__.'/router/routes.php';
+
+$core = new Core();
+core->run($routes);
