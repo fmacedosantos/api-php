9b9f3e314245fa4d57c6dfd111eb988e6c9b2599
fmacedosantos <felipemacedo986@gmail.com>
2024-10-25 23:05:11 -0300

    Criando o core de interpretação de rotas e chamadas de controller/action

--- core/Core.php
+++ core/Core.php
@@ -4,6 +4,35 @@ class Core
 {
     public function run($routes)
     {
-        
+        $url = '/';
+
+        isset($_GET['url']) ? $url .= $_GET['url'] : '';
+
+        $routerFound = false;
+
+        foreach($routes as $path => $controller)
+        {
+            $pattern = '#^'.preg_replace('/{id}/', '(\w+)', $path).'$#';
+
+            if (preg_match($pattern, $url, $mathes)) 
+            {
+                array_shift($mathes);
+
+                $routerFound = true;
+
+                [$currentController, $action] = explode('@', $controller);
+
+                require_once __DIR__."/../controllers/$currentController.php";
+
+                $newController = new $currentController();
+                $newController->$action();
+            }
+        }
+
+        if (!$routerFound) {
+            require_once __DIR__."/../controllers/NotFoundController.php";
+            $controller = new NotFoundController();
+            $controller->index();
+        }
     }
 }
